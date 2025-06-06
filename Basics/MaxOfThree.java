6789eaff77325ca264895f8225b3e41050b506f8
PALLEBOINASAHITHIYADAV <sahithisuresh14@gmail.com>
2025-06-06 10:44:39 +0530

    Update MaxOfThree.java

--- Basics/MaxOfThree.java
+++ Basics/MaxOfThree.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 public class MaxOfThree {
-   
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner  sc = new Scanner(System.in);
         System.out.print("enter first number: ");
         int a=sc.nextInt();
