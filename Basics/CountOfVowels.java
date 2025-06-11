8e4677bc3faac8cdfdbb4aad2d3f644e9a2430d1
PALLEBOINASAHITHIYADAV <sahithisuresh14@mail.com>
2025-06-11 12:42:56 +0530

    Added CountOfVowels program

---
+++ Basics/CountOfVowels.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string: ");
+        String name = sc.nextLine();
+        String lower = name.toLowerCase();
+        int count =0;
+        for(int i =0;i<=lower.length()-1;i++){
+            char ch = name.charAt(i);
+             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                 count ++;
+             }
+            // System.out.println(count);
+            
+        }
+         System.out.println(count);
+        
+        
+        sc.close();
+        }
+}
